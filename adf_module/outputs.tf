70971f20148e55637e6bb960e8defc2188e7acf7
lime <lime@limelime.local>
2023-05-16 23:29:32 +0200

    main.tf & variables.tf & outputs.tf for adf and blob

---
+++ adf_module/outputs.tf
@@ -0,0 +1,8 @@
+output "data_factory_name_out" {
+  value = resource.azurerm_data_factory.data_factory.name
+}
+
+
+
+
+
---
+++ adf_module/variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+  type        = string
+  default     = "lime"
+  description = "description"
+}
+variable "resource_group_location" {
+  type        = string
+  default     = "West Europe"
+  description = "description"
+}

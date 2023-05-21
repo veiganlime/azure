99cb594c8cdd8c7ab03d5a485fbf027ea8084b58
lime <lime@limelime.local>
2023-05-21 10:18:04 +0200

    deploying vm and network

--- main.tf
+++ main.tf
@@ -80,3 +80,55 @@ resource "azurerm_data_factory" "lime-adf" {
   location            = azurerm_resource_group.lime.location
   resource_group_name = azurerm_resource_group.lime.name
 }
+
+
+
+resource "azurerm_virtual_network" "lime-vn" {
+  name                = "reslime-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.lime.location
+  resource_group_name = azurerm_resource_group.lime.name
+}
+
+resource "azurerm_subnet" "lime-sn" {
+  name                 = "reslimesninternal"
+  resource_group_name  = azurerm_resource_group.lime.name
+  virtual_network_name = azurerm_virtual_network.lime-vn.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "lime-ni" {
+  name                = "reclimeni"
+  location            = azurerm_resource_group.lime.location
+  resource_group_name = azurerm_resource_group.lime.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.lime-sn.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "lime-vm" {
+  name                = "reslimevm"
+  resource_group_name = azurerm_resource_group.lime.name
+  location            = azurerm_resource_group.lime.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "E654550e#"
+  network_interface_ids = [
+    azurerm_network_interface.lime-ni.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
